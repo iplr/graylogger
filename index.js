5e2218634bd53e534b5b74c80358c6238ddd999c
iLia Lumpov <i@lumpov.ru>
2019-04-29 14:45:03 +0400

    0.0.15 request.debug=false

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var term = require('terminal-kit').terminal;
 var moment = require('moment');
 var stringz = require('stringz'); // for emoji support ❤️
 var request = require('request');
-request.debug = false;
 var os = require('os');
 
 var gelfLevel = new Array;
@@ -186,6 +185,8 @@ function sendHTTPGelf(logData, callback) {
     json: true,
     body: locMsg
   };
+
+  request.debug = false;
 
   request(options,
     function(error, response, body) {
